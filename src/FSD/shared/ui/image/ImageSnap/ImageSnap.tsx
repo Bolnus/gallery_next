342e77b2c392e22ed117eb0d8107f099dcf3e000
Bolnus <you@example.com>
2025-12-16 15:34:02 +0300

    feat: Retry logic for img elements fetch

--- src/FSD/shared/ui/image/ImageSnap/ImageSnap.tsx
+++ src/FSD/shared/ui/image/ImageSnap/ImageSnap.tsx
@@ -6,6 +6,9 @@ import { FileLoadState, GalleryImage } from "../../../lib/common/galleryTypes";
 import classes from "./ImageSnap.module.scss";
 import { getUnitedClassnames } from "../../../lib/common/commonUtils";
 
+const MAX_RETRIES = 3;
+const RETRY_DELAY_MS = 2000;
+
 export interface OwnProps {
   element: GalleryImage;
   albumId?: string;
@@ -27,9 +30,39 @@ function onLoad(setLocalLoadState: (state: FileLoadState) => void) {
   setLocalLoadState(FileLoadState.downloaded);
 }
 
+function initNewTry(
+  retryCountRef: React.MutableRefObject<number>,
+  setLocalLoadState: (state: FileLoadState) => void,
+  setLocalSrc: (str: string) => void,
+  newSrc: string
+) {
+  retryCountRef.current += 1;
+  setLocalLoadState(FileLoadState.downloading);
+  setLocalSrc(newSrc);
+}
+
 export function ImageSnap({ element, onClick }: Readonly<OwnProps>): JSX.Element {
   const [localLoadState, setLocalLoadState] = React.useState<FileLoadState>(element.loadState);
   const loading = localLoadState !== FileLoadState.uploaded && localLoadState !== FileLoadState.downloaded;
+  const [localSrc, setLocalSrc] = React.useState(element.url);
+  const retryCountRef = React.useRef(0);
+
+  React.useEffect(() => {
+    let timeOutId: ReturnType<typeof setTimeout>;
+    if (localLoadState === FileLoadState.downloadFailed && retryCountRef.current < MAX_RETRIES) {
+      timeOutId = setTimeout(
+        () =>
+          initNewTry(
+            retryCountRef,
+            setLocalLoadState,
+            setLocalSrc,
+            `${element.url}&retry=${retryCountRef.current + 1}`
+          ),
+        RETRY_DELAY_MS
+      );
+    }
+    return () => timeOutId && clearTimeout(timeOutId);
+  }, [localLoadState, element.url]);
 
   return (
     <div className={classes.imageWrapper} onClick={() => onImageClick(element.id, onClick)}>
@@ -49,7 +82,7 @@ export function ImageSnap({ element, onClick }: Readonly<OwnProps>): JSX.Element
           alt={element.name || ""}
           onError={() => onError(setLocalLoadState)}
           onLoad={() => onLoad(setLocalLoadState)}
-          src={element.url || ""}
+          src={localSrc || ""}
           loading="lazy"
           fill
           sizes="(min-width: 2300px) 42vw, (min-width: 1380px)
