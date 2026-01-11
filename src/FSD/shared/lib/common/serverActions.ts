c6ebe24af2326d6827fe596a101a3c3bce1b4acf
Bolnus <you@example.com>
2026-01-11 15:43:50 +0300

    fix: Path invalidation

--- src/FSD/shared/lib/common/serverActions.ts
+++ src/FSD/shared/lib/common/serverActions.ts
@@ -1,7 +1,10 @@
 "use server";
 import { revalidatePath } from "next/cache";
+// import { routing } from "../../../../app/request";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function revalidateAlbum(id: string): Promise<void> {
-  revalidatePath(`/album/${id}`);
+  // for (const locale of routing.locales) {
+  revalidatePath(`/[locale]/album/${id}`);
+  // }
 }
