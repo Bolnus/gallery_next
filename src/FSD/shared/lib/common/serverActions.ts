4949bb253c6c42fda6914ec05898208a1c293443
Bolnus <you@example.com>
2026-01-12 14:57:31 +0300

    fix: Path invalidation 2

--- src/FSD/shared/lib/common/serverActions.ts
+++ src/FSD/shared/lib/common/serverActions.ts
@@ -1,10 +1,10 @@
 "use server";
 import { revalidatePath } from "next/cache";
-// import { routing } from "../../../../app/request";
+import { routing } from "../../../../app/request";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function revalidateAlbum(id: string): Promise<void> {
-  // for (const locale of routing.locales) {
-  revalidatePath(`/[locale]/album/${id}`);
-  // }
+  for (const locale of routing.locales) {
+    revalidatePath(`/${locale}/album/${id}`);
+  }
 }
--- src/app/api/cron/routes.ts
+++ src/app/api/cron/routes.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export async function GET(): Promise<unknown> {
-  revalidatePath("[locale]/main/[pageNumber]");
+  revalidatePath("/[locale]/main/[pageNumber]");
   return NextResponse.json({ success: true });
 }
