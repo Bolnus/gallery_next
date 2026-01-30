a0de9c2df558ac7c88eea221569a2e3efd14147f
Bolnus <you@example.com>
2026-01-30 11:21:06 +0300

    fix: Pagination controls style

--- src/FSD/widgets/PaginationV2/ui/PaginationV2.tsx
+++ src/FSD/widgets/PaginationV2/ui/PaginationV2.tsx
@@ -5,6 +5,7 @@ import { ButtonIcon } from "../../../shared/ui/button/ButtonIcon/ButtonIcon";
 import { IconName } from "../../../shared/ui/icons/ReactIcon/types";
 import { ButtonIconBackground } from "../../../shared/ui/button/ButtonIcon/types";
 import { UiSize } from "../../../shared/lib/common/commonTypes";
+import { getUnitedClassnames } from "../../../shared/lib/common/commonUtils";
 
 interface PaginationProps {
   page: number;
@@ -26,45 +27,54 @@ export function PaginationV2({
   const intl = useTranslations("Pagination");
   const pageRounded = Math.floor(page);
   const prevPagesAmount = (pageRounded - 1) * pageSize;
+  const lastPageNumber = Math.floor(albumsCount / pageSize) + 1;
+  const isManyPages = lastPageNumber > 2;
+
   return (
     <div className={classes.pagination}>
-      <div className={classes.pagination__label}>
+      <div className={classes.pagination__labelBlock}>
         {isFetching ? (
           <SkeletonLoader />
         ) : (
-          <span className="singleLine">
+          <span className={getUnitedClassnames(["singleLine", classes.pagination__label])}>
             {`${prevPagesAmount + 1} - ${prevPagesAmount + loadedAlbumsNumber} ${intl("outOf")} ${albumsCount}`}
           </span>
         )}
       </div>
-      <ButtonIcon
-        iconName={IconName.ChevronDoubleLeft}
-        background={ButtonIconBackground.Transparent}
-        size={UiSize.SmallAdaptive}
-        onClick={() => onPageSelect(1)}
-      />
+      {isManyPages && (
+        <ButtonIcon
+          iconName={IconName.ChevronDoubleLeft}
+          background={ButtonIconBackground.Transparent}
+          size={UiSize.SmallAdaptive}
+          onClick={() => onPageSelect(1)}
+          isFetching={isFetching}
+        />
+      )}
       <ButtonIcon
         iconName={IconName.ChevronLeft}
         background={ButtonIconBackground.Transparent}
         size={UiSize.SmallAdaptive}
         onClick={() => onPageSelect(pageRounded <= 2 ? 1 : pageRounded - 1)}
+        isFetching={isFetching}
       />
       <ButtonIcon
         iconName={IconName.ChevronRight}
         background={ButtonIconBackground.Transparent}
         size={UiSize.SmallAdaptive}
         onClick={() =>
-          onPageSelect(
-            pageRounded > Math.floor(albumsCount / pageSize) ? Math.floor(albumsCount / pageSize) + 1 : pageRounded + 1
-          )
+          onPageSelect(pageRounded > Math.floor(albumsCount / pageSize) ? lastPageNumber : pageRounded + 1)
         }
+        isFetching={isFetching}
       />
-      <ButtonIcon
-        iconName={IconName.ChevronDoubleRight}
-        background={ButtonIconBackground.Transparent}
-        size={UiSize.SmallAdaptive}
-        onClick={() => onPageSelect(Math.floor(albumsCount / pageSize) + 1)}
-      />
+      {isManyPages && (
+        <ButtonIcon
+          iconName={IconName.ChevronDoubleRight}
+          background={ButtonIconBackground.Transparent}
+          size={UiSize.SmallAdaptive}
+          onClick={() => onPageSelect(lastPageNumber)}
+          isFetching={isFetching}
+        />
+      )}
     </div>
   );
 }
