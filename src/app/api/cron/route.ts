dbd1d195723c91a08f81f433ac64c4dd8a509a44
Bolnus <you@example.com>
2026-02-02 13:47:43 +0300

    feat: New pages revalidation in vercel

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,6 +2,8 @@ import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export function GET(): Response {
+  revalidatePath("/[locale]/categories", "page");
   revalidatePath("/[locale]/main/[pageNumber]", "page");
+  revalidatePath("/[locale]/categories/[category]/[pageNumber]", "page");
   return NextResponse.json({ success: true });
 }
