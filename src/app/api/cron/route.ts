156341e38a9a838970a48802731a1f3893a9c373
Bolnus <you@example.com>
2026-02-06 18:34:17 +0300

    fix: Cron jobs

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,8 +2,22 @@ import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export function GET(): Response {
-  revalidatePath("/[locale]/categories", "page");
-  revalidatePath("/[locale]/main/[pageNumber]", "page");
-  revalidatePath("/[locale]/categories/[category]/[pageNumber]", "page");
-  return NextResponse.json({ success: true });
+  try {
+    console.log(`CRON STARTED ${new Date().toISOString()}`);
+    revalidatePath("/[locale]/categories", "page");
+    revalidatePath("/[locale]/main/[pageNumber]", "page");
+    revalidatePath("/[locale]/categories/[category]/[pageNumber]", "page");
+    console.log(`CRON FINISHED ${new Date().toISOString()}`);
+    return NextResponse.json({ success: true });
+  } catch (localError) {
+    console.warn(localError);
+    return NextResponse.json(
+      {
+        success: false,
+        timestamp: new Date().toISOString(),
+        error: localError instanceof Error ? localError.message : "Unknown error"
+      },
+      { status: 500 }
+    );
+  }
 }
